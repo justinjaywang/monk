7a2507b3a320d1ca149731354f2d944e30da818d
justinjaywang <justinjaywang@gmail.com>
2014-10-15 19:34:08 -0700

    modified container and figure sizing; added other styles

--- source/js/all.js
+++ source/js/all.js
@@ -1,12 +1,28 @@
 +function ($) {
   'use strict';
 
-  // wrap images in a figure and append figcaption
-  $('.post-content').each(function (i) {
-    $(this).find('img').each(function () {
-      $(this).wrap('<figure class="figure"></figure>');
-      var alt = this.alt;
-      if (alt) $(this).parent().append('<figcaption class="figure-caption">' + alt + '</figcaption>');
+  $('.post-content').each(function (i, postContent) {
+    // (1) remove container classes from .post-content
+    $(postContent).each(function () {
+      $(this).removeClass('container container--narrow');
+    });
+    // (2) add container--narrow to each of the children
+    $(postContent).children().each(function () {
+      $(this).addClass('container container--narrow');
+    });
+    // (3) wrap images in a figure and append figcaption
+    $(postContent).find('img').each(function (j, image) {
+      var $imageWrap = $(image).parent();
+      if ($imageWrap.hasClass('container')) {
+        $imageWrap.find('br').remove(); // remove extra <br>
+        $(image).unwrap(); // remove .container wrap performed in (2)
+      }
+      $(image).wrap('<figure class="post-figure container container--wide"></figure>');
+      var alt = image.alt;
+      var $figure = $(image).parent();
+      if (alt) {
+        $figure.append('<figcaption class="post-figure-caption">' + alt + '</figcaption>');
+      }
     });
   });
 
